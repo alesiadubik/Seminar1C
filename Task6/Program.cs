4dea5d0e38db8060e7495f03138cb5f1db4667fa
alesiadubik <alek80d@gmail.com>
2022-11-21 20:48:16 +0300

    Add task6

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number % 2) == 0)
+{
+    Console.WriteLine("Чётное число");
+}
+else
+{
+    Console.WriteLine("Нечётное число");
+}
