3bda7aaa870a7aff9ef82e32513f3149f745cabc
alesiadubik <alek80d@gmail.com>
2022-11-21 21:04:31 +0300

    Add task8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i<= number; i++)
+{
+    if ((i%2) ==0)
+    {
+      Console.Write(i+", ");  
+    }
+}
