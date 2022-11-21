bc0fe9389a0f097dcd33577bbe24292f16dd4106
alesiadubik <alek80d@gmail.com>
2022-11-21 20:35:48 +0300

    Add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите три числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine("max =" + number1);
+}
+else if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine("max =" + number2);
+}
+else
+{
+    Console.WriteLine("max =" + number3);
+}
