ac36209014407a03003c1ff73bb8d6c3a65d091e
alesiadubik <alek80d@gmail.com>
2022-12-11 23:51:45 +0300

    Add hw 6(task 41, 43)

---
+++ Task100/Program.cs
@@ -0,0 +1,30 @@
+﻿// задача 1 - HARD необязательная. На вход программы подаются одно целое число. Составьте список чисел Фибоначчи,
+//  в том числе для отрицательных индексов. Это по сути последовательность Негафибоначчи.
+// Пример:
+// для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+Console.WriteLine("Введите число");
+double NegaFibonacci(int n)
+{
+    if (n == -1) return 1;
+    else if (n == -2) return -1;
+    else return NegaFibonacci(n + 2) - NegaFibonacci(n + 1);
+}
+
+for (int j = -8; j <= -1; j++)  //считаем числа фиб
+    {
+    
+        Console.Write(NegaFibonacci(j)+ "," );
+
+    }
+Console.Write(0+"," );
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2); //в противном случае возвр фибоначи (n-1) + (n-2)
+}
+for (int i = 1; i <= 8; i++)  //считаем числа фиб
+{
+    Console.Write(Fibonacci(i)+"," );
+}
---
+++ Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int[] CreatArray()
+{
+Console.WriteLine("Введите количество чисел");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа");
+int[] nums = new int [n];
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+}
+return nums;
+}
+
+void CountArray ()
+{
+int[] nums = CreatArray();    
+int count = 0; 
+foreach (int i in nums)
+    if (i>0)
+        count++;
+string str = string.Join(", ", nums);
+Console.WriteLine(str);
+Console.WriteLine($"Количество чисел>0 = {count}");
+}
+CountArray();
+
+/*Console.WriteLine("Введите количество чисел");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа");
+int[] nums = new int [n];
+int count = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+    if (nums[i]>0)
+    {
+        count++;
+    }
+}
+string str = string.Join(", ", nums);
+Console.WriteLine(str);
+Console.WriteLine($"Количество чисел>0 {count}");*/
