48dd73c1d1da0c052e514b1be922b0118f450862
alesiadubik <alek80d@gmail.com>
2022-12-20 00:06:04 +0300

    Add HW 8 seminar

---
+++ Task54/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив: В итоге получается вот такой массив:
+1 4 7 2                 7 4 2 1
+5 9 2 3                 9 5 3 2
+8 4 2 4                 8 4 4 2 */ 
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i =0; i < array.GetLength(0); i++)
+    {
+        for (int j =0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i,j]} ");
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите количетсво строк массива");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количетсво столбцов массива");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[a, b];
+FillArray (array);
+PrintArray(array);
+
+void ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1)-1; k++)
+            {
+                if (array[i,k]<array[i,k+1])
+                {
+                    int temp = array[i, k+1];
+                    array[i,k+1]=array[i,k];
+                    array[i,k]=temp;
+                }
+            }
+        }
+    }
+}
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
